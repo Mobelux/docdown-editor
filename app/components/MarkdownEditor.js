92a6721fa04b55c922ec480b0d9a711fbfd8fa72
Daniel Farrell <daniel@mobelux.com>
2016-10-28 13:55:16 -0400

    WIP on getting a prism editor

--- app/components/MarkdownEditor.js
+++ app/components/MarkdownEditor.js
@@ -1,17 +1,53 @@
+import 'prismjs';
+import 'prismjs/components/prism-markdown';
 import React from 'react';
-import { Editor, EditorState } from 'draft-js';
+import { Editor, EditorState, convertFromRaw } from 'draft-js';
+import PrismDecorator from 'draft-js-prism';
+
+function plainRender(props) {
+  props = extend({}, props, {
+    className: 'token ' + props.type
+  });
+
+  return React.createElement(
+    "span",
+    props,
+    props.children
+  );
+}
+
+const options = {
+  defaultSyntax: 'markdown',
+  filter: () => true,
+  getSyntax: () => 'markdown',
+  render: plainRender
+};
+const decorator = new PrismDecorator(options);
 
 class MarkdownEditor extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { editorState: EditorState.createEmpty() };
+
+    const initialContent = {
+      entityMap: {},
+      blocks: [{
+        type: 'code-block',
+        text: props.text
+      }]
+    };
+
+    const contentState = convertFromRaw(initialContent);
+
+    this.state = { editorState: EditorState.createWithContent(contentState, decorator) };
     this.onChange = editorState => this.setState({ editorState });
   }
   render() {
     const { editorState } = this.state;
     return (
       <div className="flex-1-50">
-        <Editor editorState={editorState} onChange={this.onChange} />
+        <pre className="language-markdown">
+          <Editor editorState={editorState} onChange={this.onChange} />
+        </pre>
       </div>
     );
   }
