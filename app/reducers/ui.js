50ff63abed42b012350d83b73214a21ecdd497d2
swainea <swainea@gmail.com>
2016-11-21 11:51:33 -0500

    stupid syntax error

--- app/reducers/ui.js
+++ app/reducers/ui.js
@@ -17,8 +17,8 @@ const uiReducer = handleActions({
     return state.merge({ sidebarVisible: !state.get('sidebarVisible'), sidebarSize });
   },
   [PANE_TOGGLE]: (state) => {
-    // this logic is wrong for the default state - removing the right element but makign width of pane 1 0
-    const paneSize = !state.get('paneSize') ? (document.getElementById('split-pane-wrapper') / 2) : 0;
+    // this logic is not exactly right - it will never actually be 0... but it is working
+    const paneSize = !state.get('paneVisible') ? 0 : 100 + '%';
     return state.merge({ paneVisible: !state.get('paneVisible'), paneSize });
   },
   [PANE_RESIZE]: (state, { payload }) => (state.set('paneSize', payload)),
