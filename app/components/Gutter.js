38b3af0ee78e422372cc1913a94907196018d4e0
swainea <swainea@gmail.com>
2016-11-29 14:57:20 -0500

    basic styling and removing unused props

--- app/components/Gutter.js
+++ app/components/Gutter.js
@@ -3,35 +3,24 @@ import React, { Component, PropTypes } from 'react';
 export default class Gutter extends Component {
   static propTypes = {
     text: PropTypes.string,
-    list: PropTypes.object,
     listItem: PropTypes.object,
-    style: PropTypes.object,
-    styleList: PropTypes.object,
-    styleListItem: PropTypes.object,
     children: PropTypes.object
   }
 
-  static defaultProps = {
-    style: { border: '1px solid black' },
-    styleEditor: {},
-    styleList: { background: '#eee' },
-    styleListItem: {}
-  }
-
   render() {
-    const { text, style, styleList, styleListItem, list, listItem, children } = this.props;
+    const { text, listItem, children } = this.props;
 
     return (
-      <div style={{ ...style, display: 'flex' }}>
+      <div style={{ display: 'flex' }}>
         <ol
-          {...list}
-          style={{ ...styleList, margin: 0, padding: 0 }}
+          className="gutter"
         >
           {[...Array(text.split('\n').length)].map((x, i) =>
             <li
+              className="gutter__num"
               key={i}
               {...listItem}
-              style={{ ...styleListItem, listStylePosition: 'inside' }}
+              style={{ listStylePosition: 'inside' }}
             />
           )}
         </ol>
