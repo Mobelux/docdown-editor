4c5dfe2d9ae79724adb352336d78139f5335a606
swainea <swainea@gmail.com>
2016-11-30 13:16:58 -0500

    more refined sketch with notes

--- app/components/Tabs.js
+++ app/components/Tabs.js
@@ -1,24 +1,70 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, cx } from 'react';
 
 class Tabs extends React.Component {
   static PropTypes = {
+    // not sure if this is necessary
     children: PropTypes.object
+  }
+
+  constructor(props) {
+    // so props are not undefined
+    super(props);
+    // rebinding
+    this.handleTabClick = ::this.handleTabClick;
+    // initializing state
+    this.state = {
+      // no selection intially
+      selected: 0
+    };
+  }
+
+  // event handler for tab click - will need an onClick on the individual Tab componenet
+  handleTabClick(e) {
+    // dataset holds the index
+    const { tabid } = e.target.dataset;
+    // changes the state and rerenders. Selected is now a number from the tabId
+    this.setState({ selected: parseInt(tabid) });
   }
 
   render() {
     const { children } = this.props;
+    let tabs;
+    let tabContents;
+
+    // maps over the children to generate a list of tabs and tab content
+    React.Children.forEach(children, (tab, idx) => (
+      // creates an array of tabs
+      tabs.push(
+        <li
+          // tab-view__tab is the static className, and if this.state.selected === idk then
+          className={cx('tab-view__tab', { 'is-active': this.state.selected === idx })}
+          // el.dataset.tabID === idx
+          data-tabId={idx}
+          onClick={this.handleTabClick}
+        >
+          { tab.props.name }
+        </li>
+      )
+      tabContents.push(
+        <div
+          // tab-view__tab-contents is hidden if state is not equal to idx
+          className={cx('tab-view__tab-contents'}, {'is-hidden': this.state.selected !== idx })
+        >
+          { tab }
+        </div>
+      )
+    ));
 
     return (
-      <div>
+      <div className ="tab-view">
         <ol
-          className="tabs-list"
+          className="tab-view__tabs"
         >
-        // will need logic here to add/remove lis for tabs onClick
-          <li className="Tab" />
+          { tabs }
         </ol>
-        <div className="TabsPanel">
+        <div className="tab-view__content">
         // here is where we need everything thine #split-pane-wrapper to render
-          {children}
+          { tabContents }
         </div>
       </div>
     );
