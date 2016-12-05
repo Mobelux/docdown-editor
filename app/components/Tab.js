4db2af2a2fe69c45a6f73b10e60c2eea38f3c31f
swainea <swainea@gmail.com>
2016-12-05 17:23:04 -0500

    scaffolding for container and component for tabs

--- app/components/Tab.js
+++ app/components/Tab.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
-class Tab extends React.Component {
-  render() {
-    return (
-      <li>
-        tab name goes here
-      </li>
-    );
-  }
-}
+// will probably need an onClick to close and a way to indicate Changed=true in future
+const Tab = ({ id, name, changed }) => (
+  <li>
+    <a href={`#${name}`}>
+      <span>{name}</span>
+    </a>
+  </li>
+);
+
+Tab.propTypes = {
+  id: PropTypes.number,
+  name: PropTypes.string,
+  changed: PropTypes.bool
+};
 
 export default Tab;
--- app/containers/Tabs.js
+++ app/containers/Tabs.js
@@ -1,18 +1,55 @@
 import React from 'react';
 import Tab from '../components/Tab';
 
-class Tabs extends React.Component {
+class Tabs extends React.PureComponent {
   static propTypes = {
-    // I am assuming what I need here are the ID and Name
+    files: PropTypes.object,
+    handleFile: PropTypes.func
+  }
+
+  constructor(props){
+    super(props);
+    // I am not sure about the state
   }
 
   render() {
+    // names does not exist yet but it will - these are from the fiels.js reducer
+    const { files } = this.props;
+    // will there ever be an instance in which there is no files?
+    // I think a blank new one should be created - maybe that needs to be returned instead of null
+    if (!files) {
+      return null;
+    }
+
+    // this is where I want to map over each file and return the id/name/and changed
+
+    // then I want to make and render the data from the maps
     return (
-      <ul className="tabs">
-        <Tab />
+      <ul className="tabs__list">
+        {files.map(f =>
+          <Tab
+            id={f.get('id')}
+            name={f.get('name')}
+            changes={f.get('changed')}
+            className="tabs__item"
+          />
+        )})
       </ul>
     );
   }
 }
 
-export default Tabs;
+// this is where I need to get the state of the files from files.js reducer and pass in as props to a render method
+
+// value of mapStateToProps will be an object derived from state (as it lives in the store),
+// whose keys will be passed to your target component (the component connect is applied to) as props.
+const mapStateToProps = state => ({
+  files: state.files.get('files')
+});
+
+//passing the file ID to actions?
+const mapDispatchToProps = dispatch => ({
+  handleFile: bindActionCreators(openFile, dispatch)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Tabs);
