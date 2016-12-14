0ccf9b2d7c98c0da7210ac5eb4431d8586e305fe
Daniel Farrell <daniel@mobelux.com>
2016-12-14 11:29:08 -0500

    Adjust the other pains on sidebar toggle

--- app/reducers/ui.js
+++ app/reducers/ui.js
@@ -8,12 +8,22 @@ import { FOLDER_OPEN } from '../actions/files';
 const initialState = Map({
   sidebarVisible: false,
   paneVisible: true,
-  paneSize: ((document.documentElement.clientWidth - 250) / 2),
+  paneSize: (document.documentElement.clientWidth / 2),
   sidebarSize: 250
 });
 
 const uiReducer = handleActions({
-  [SIDEBAR_TOGGLE]: state => (state.set('sidebarVisible', !state.get('sidebarVisible'))),
+  [SIDEBAR_TOGGLE]: (state) => {
+    const sidebarVisible = !state.get('sidebarVisible');
+    const sidebarSize = state.get('sidebarSize');
+    let paneSize = state.get('paneSize');
+    if (sidebarVisible) {
+      paneSize -= (sidebarSize / 2);
+    } else {
+      paneSize += (sidebarSize / 2);
+    }
+    return state.merge({ sidebarVisible, paneSize });
+  },
   [FOLDER_OPEN]: state => (state.set('sidebarVisible', true)),
   [PANE_TOGGLE]: state => (state.set('paneVisible', !state.get('paneVisible'))),
   [PANE_RESIZE]: (state, { payload }) => (state.set('paneSize', payload)),
