093004224edc97bf1a322756bb40597565618ed9
Daniel Farrell <daniel@mobelux.com>
2016-12-22 13:48:42 -0500

    Allow dragging files/folders onto the app
    Doesn't work on the dock icon yet

--- app/index.js
+++ app/index.js
@@ -1,9 +1,12 @@
+/* global document */
+import fs from 'fs';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import App from './containers/App';
 import Replacer from './components/Replacer';
+import { openFolder, openFile } from './actions/files';
 
 const store = configureStore();
 
@@ -24,3 +27,22 @@ if (replacer) {
     replacer
   );
 }
+
+document.addEventListener('drop', (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  for (const f of e.dataTransfer.files) {
+    const stat = fs.statSync(f.path);
+    if (stat.isFile()) {
+      store.dispatch(openFile(f.path));
+    }
+    if (stat.isDirectory()) {
+      store.dispatch(openFolder(f.path));
+    }
+  }
+  return false;
+});
+document.addEventListener('dragover', (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+});
