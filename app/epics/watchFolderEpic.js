9a67a2e1c6d67f15b79e565deb1f6517f1c43b68
Daniel Farrell <daniel@mobelux.com>
2017-01-09 10:16:03 -0500

    Write test for the watchFolderEpic

--- app/epics/watchFolderEpic.js
+++ app/epics/watchFolderEpic.js
@@ -1,8 +1,3 @@
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/bufferTime';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/takeUntil';
 import { FOLDER_OPEN, FOLDER_CLOSE, changeFolder, closeFolder } from '../actions/folder';
 
 const watchOptions = {
---
+++ test/epics/watchFolderEpic.spec.js
@@ -0,0 +1,35 @@
+import expect, { createSpy } from 'expect';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/bufferTime';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/takeUntil';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/toPromise';
+import { Observable } from 'rxjs/Observable';
+import { ActionsObservable } from 'redux-observable';
+import watchFolderEpic from '../../app/epics/watchFolderEpic';
+import { FOLDER_CHANGE, openFolder } from '../../app/actions/folder';
+
+describe('watchFolderEpic', () => {
+  it('should watch folder and merge observable', () => {
+    const addDirEvent = { event: 'addDir', name: '/tmp/' };
+    const addEvent = { event: 'add', name: '/tmp/file.md' };
+    const fileEvents = Observable.of(addDirEvent, addEvent);
+    const watch = createSpy().andReturn(fileEvents);
+    const action$ = ActionsObservable.of(openFolder('/tmp/'));
+    watchFolderEpic(action$, { watch })
+      .delay(200)
+      .toPromise()
+      .then((actionReceived) => {
+        expect(watch.calls.length).toExist();
+        expect(watch.calls[0].arguments).toEqual(['/tmp/', {
+          ignorePermissionErrors: true,
+          ignored: /(^|[/\\])\../
+        }]);
+        expect(actionReceived.type).toBe(FOLDER_CHANGE);
+        expect(actionReceived.payload).toEqual([addDirEvent, addEvent]);
+      });
+  });
+});
